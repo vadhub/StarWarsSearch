ab8752408e5300e5e9e8d184f0113e20c98f5850
vadhub <gabderahmanov99@gmail.com>
2022-06-30 12:39:58 +0300

    create retrofit instance

---
+++ app/src/main/java/com/vad/starwarssearch/data/remote/RetrofitInstance.kt
@@ -0,0 +1,28 @@
+package com.vad.starwarssearch.data.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object {
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl("https://swapi.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        val api by lazy {
+            retrofit.create(CharacterService::class.java)
+        }
+    }
+}
