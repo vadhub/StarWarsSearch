310c2bc1cbad450f27c10c628608d3ef6556911b
vadhub <gabderahmanov99@gmail.com>
2022-06-30 12:38:54 +0300

    navigation settings

--- app/src/main/java/com/vad/starwarssearch/presentation/MainActivity.kt
+++ app/src/main/java/com/vad/starwarssearch/presentation/MainActivity.kt
@@ -2,11 +2,21 @@ package com.vad.starwarssearch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.vad.starwarssearch.R
+import com.vad.starwarssearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.bottomNavigationView.setupWithNavController(binding.starWarsNavHostFragment.findNavController())
     }
 }
