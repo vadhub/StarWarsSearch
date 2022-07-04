e7d2a51b7aebfb7623b89a3d2166c202dc6a2f2c
vadhub <gabderahmanov99@gmail.com>
2022-07-04 14:03:26 +0300

    correct navigation setup

--- app/src/main/java/com/vad/starwarssearch/presentation/MainActivity.kt
+++ app/src/main/java/com/vad/starwarssearch/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.vad.starwarssearch.R
 import com.vad.starwarssearch.data.local.AppDatabase
@@ -12,19 +13,17 @@ import com.vad.starwarssearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     lateinit var viewModel: CharacterViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_main)
 
         val characterRepository = CharacterRepository(AppDatabase.getDatabase(this))
         val viewModelFactory = CharacterViewModelFactory(characterRepository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(CharacterViewModel::class.java)
 
-        binding.bottomNavigationView.setupWithNavController(binding.starWarsNavHostFragment.findNavController())
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.starWarsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
