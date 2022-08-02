6363cafc277e533c8f97053947cc35c7a1f1282b
vadhub <gabderahmanov99@gmail.com>
2022-08-02 12:18:48 +0300

    set search function

--- app/src/main/java/com/vad/starwarssearch/presentation/character/CharacterFragment.kt
+++ app/src/main/java/com/vad/starwarssearch/presentation/character/CharacterFragment.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
-import android.widget.Toolbar
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vad.starwarssearch.R
+import com.vad.starwarssearch.data.entity.Character
 import com.vad.starwarssearch.databinding.FragmentCharacterBinding
 import com.vad.starwarssearch.domain.Resource
 import com.vad.starwarssearch.presentation.CharacterViewModel
 import com.vad.starwarssearch.presentation.MainActivity
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CharacterFragment : Fragment() {
 
@@ -21,7 +23,6 @@ class CharacterFragment : Fragment() {
     private lateinit var binding: FragmentCharacterBinding
     private lateinit var viewModel: CharacterViewModel
     private lateinit var characterAdapter: CharacterAdapter
-    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -87,25 +88,42 @@ class CharacterFragment : Fragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
         menu.clear()
         inflater.inflate(R.menu.menu_search, menu)
 
-        val searchView = ((context as MainActivity).supportActionBar?.themedContext ?: context)?.let { SearchView(it) }
+        val searchView = ((context as MainActivity).supportActionBar?.themedContext
+            ?: context)?.let { SearchView(it) }
         menu.findItem(R.id.search_character).apply {
             setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW or MenuItem.SHOW_AS_ACTION_IF_ROOM)
             actionView = searchView
         }
 
+        val listCharacters = mutableListOf<Character>()
+
         searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                TODO("Not yet implemented")
+
+                return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                TODO("Not yet implemented")
-            }
+                val searchText = newText!!.lowercase(Locale.getDefault())
+                if (searchText.isNotEmpty()) {
+                    listCharacters.clear()
+                    characterAdapter.differ.currentList.forEach {
+                        if (it.name.lowercase(Locale.getDefault()).contains(searchText)) {
+                            listCharacters.add(it)
+                        }
+                    }
 
+                    if (listCharacters.isNotEmpty()) {
+                        characterAdapter.differ.submitList(listCharacters)
+                    }
+                }
+                return false
+            }
         })
+
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
