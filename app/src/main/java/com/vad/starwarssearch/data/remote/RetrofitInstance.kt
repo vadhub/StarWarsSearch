1cf075638b8b11ee8660908e650cdf206a32b29a
vadhub <gabderahmanov99@gmail.com>
2022-08-04 16:12:16 +0300

    changed serialize objects

--- app/src/main/java/com/vad/starwarssearch/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/vad/starwarssearch/data/remote/RetrofitInstance.kt
@@ -1,5 +1,10 @@
 package com.vad.starwarssearch.data.remote
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,24 +13,28 @@ import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
     companion object {
+
+        @OptIn(ExperimentalSerializationApi::class)
         private val retrofit by lazy {
             val logging = HttpLoggingInterceptor()
+            val contentType = "application/json".toMediaType()
+
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
             val client = OkHttpClient.Builder()
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(15, TimeUnit.SECONDS)
                 .addInterceptor(logging)
                 .build()
 
             Retrofit.Builder()
                 .baseUrl("https://swapi.dev/")
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(Json.asConverterFactory(contentType))
                 .client(client)
                 .build()
         }
 
         val api by lazy {
-            retrofit.create(CharacterService::class.java)
+            retrofit.create(CharacterApi::class.java)
         }
     }
 }
