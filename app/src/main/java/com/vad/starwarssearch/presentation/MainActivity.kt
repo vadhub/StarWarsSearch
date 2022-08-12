00efd006fdaa7a1c11928af88d68313252143513
vadhub <gabderahmanov99@gmail.com>
2022-08-12 12:49:40 +0300

    fix switch fragment

--- app/src/main/java/com/vad/starwarssearch/presentation/MainActivity.kt
+++ app/src/main/java/com/vad/starwarssearch/presentation/MainActivity.kt
@@ -6,20 +6,22 @@ import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vad.starwarssearch.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.starWarsNavHostFragment) as NavHostFragment
-        navController = navHostFragment.navController
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.characterFragment, R.id.favoriteFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
         findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(navController)
     }
 }
